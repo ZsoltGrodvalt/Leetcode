a4a616c9f26083567470cc381977f75da58ab25d
ZsoltGrodvalt <grodvalt.zsolt@gmail.com>
2024-09-05 22:19:42 +0200

    two sum

---
+++ 1. Two Sum.py
@@ -0,0 +1,18 @@
+def solution(nums, target):
+    size = len(nums)
+    for idx1 in range(size):
+        for idx2 in range(idx1+1,size):
+            if ((nums[idx1]+nums[idx2])==target):
+                return [idx1,idx2]
+
+nums = [2,7,11,15]
+target = 9
+print(solution(nums, target))
+
+nums = [3,2,4]
+target = 6
+print(solution(nums, target))
+
+nums = [3,3]
+target = 6
+print(solution(nums, target))
