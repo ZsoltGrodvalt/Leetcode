ded420795b407729c89e1db289c10c03355abc4f
ZsoltGrodvalt <grodvalt.zsolt@gmail.com>
2024-09-05 22:35:49 +0200

    initial solution

---
+++ 9. Palindrome Number.py
@@ -0,0 +1,21 @@
+# Given an integer x, return true if x is a palindrome, and false otherwise.
+
+def isPalindrome(x):
+    string = str(x)
+    print(string)
+    size = len(string)
+    print(size)
+    idx = 0
+    while idx <= (size/2):
+        if string[idx] != string[size-idx-1]:
+            return False
+    return True
+
+x = 121
+print(isPalindrome(x))
+
+x = -121
+print(isPalindrome(x))
+
+x = 12111
+print(isPalindrome(x))
