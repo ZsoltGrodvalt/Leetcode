bd2bfd527157ac225dc7ed5df0b9df6df463ca4c
ZsoltGrodvalt <grodvalt.zsolt@gmail.com>
2024-09-06 09:12:43 +0200

    Dictionary

--- 1. Two Sum.py
+++ 1. Two Sum.py
@@ -1,5 +1,7 @@
 # Return the indeces of the two numbers whose sum is the target. They have to be different indeces.
-def solution(nums, target):
+
+# Comparing all the numbers...
+def solution1(nums, target):
     size = len(nums)
     for idx1 in range(size):
         diff = target - nums[idx1]
@@ -7,14 +9,24 @@ def solution(nums, target):
             if (nums[idx2]==diff):
                 return [idx1,idx2]
 
+# Using a dictionary and only iterating through the list once
+def solution2(nums, target):
+    dict = {}
+    for idx, num in enumerate(nums):
+        difference = target - num
+        if difference in dict:
+            return [idx, dict[difference]]
+        else:
+            dict[num] = idx
+
 nums = [2,7,11,15]
 target = 9
-print(solution(nums, target))
+print(solution1(nums, target))
 
 nums = [3,2,4]
 target = 6
-print(solution(nums, target))
+print(solution1(nums, target))
 
 nums = [3,3]
 target = 6
-print(solution(nums, target))
+print(solution1(nums, target))
