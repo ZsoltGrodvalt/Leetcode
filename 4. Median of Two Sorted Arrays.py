aaa7b85b35b7ce0a254674e2f3476ca64443d3e4
ZsoltGrodvalt <grodvalt.zsolt@gmail.com>
2024-09-08 18:58:52 +0200

    nothing has changed

--- 4. Median of Two Sorted Arrays.py
+++ 4. Median of Two Sorted Arrays.py
@@ -1,22 +1,17 @@
 # Method 1: just do it, but not using the fact that they are sorted already
-
-# def findMedianSortedArrays(nums1, nums2):
-#     nums_raw = nums1 + nums2
-#     nums = sorted(nums_raw)
-#     l = len(nums)
-#     if l % 2 == 0:
-#         # even
-#         return (nums[int(l/2-1)] + nums[int(l/2)]) / 2
-#     else:
-#         # odd
-#         return nums[l//2]
-
-
-# Method 2:
 def findMedianSortedArrays(nums1, nums2):
-    return 0
+    nums_raw = nums1 + nums2
+    nums = sorted(nums_raw)
+    l = len(nums)
+    if l % 2 == 0:
+        # even
+        return (nums[int(l/2-1)] + nums[int(l/2)]) / 2
+    else:
+        # odd
+        return nums[l//2]
+
 # Test cases:
-a=[1,3]
-b=[2]
+a=[1,1,1,3]
+b=[2,6,8]
 
 print(findMedianSortedArrays(a,b))
