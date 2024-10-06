5620467c35431ba157a8f9f7cfe935492c73abbb
ZsoltGrodvalt <grodvalt.zsolt@gmail.com>
2024-10-06 18:39:33 +0200

    Sunday slay

--- df_examples.py
+++ df_examples.py
@@ -1,3 +1,5 @@
+import pandas as pd
+
 employee = pd.DataFrame({   'id':[1,2,3,4],
                             'name':['Joe','Henry','Sam','Max'],
                             'salary':[70000,80000,60000,90000],
@@ -33,3 +35,23 @@ department = pd.DataFrame({   'id':[1,2],
 
 scores = pd.DataFrame({ 'id':[1,2,3,4,5,6],
                         'score':[3.50,3.65,4.00,3.85,4.00,3.65]})
+
+activity = pd.DataFrame({   'player_id':[1,1,2,3,3],
+                            'device_id':[2,2,3,1,4],
+                            'event_date':['2016-03-01','2016-05-02','2017-06-25','2016-03-02','2018-07-03'],
+                            'games_played':[5,6,1,0,5]})
+
+teacher = pd.DataFrame({ 'teacher_id':[1,1,1,2,2,2,2],
+                        'subject_id':[2,2,3,1,2,3,4],
+                        'dept_id':[3,4,3,1,1,1,1]})
+
+
+courses = pd.DataFrame({ 'student':['A','B','C','D','E','F','G','H','I'],
+                        'class':['Math','English','Math','Biology','Math','Computer','Math','Math','Math']})
+
+daily_sales = pd.DataFrame({   'date_id':['2020-12-8','2020-12-8','2020-12-8','2020-12-7','2020-12-7','2020-12-8',
+                                          '2020-12-8','2020-12-7','2020-12-7','2020-12-7'],
+                            'make_name':['toyota','toyota','toyota','toyota','toyota','honda',
+                                          'honda','honda','honda','honda'],
+                            'lead_id':[0,1,1,0,0,1,2,0,1,2],
+                            'partner_id':[1,0,2,2,1,2,1,1,2,1]})
